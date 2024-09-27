35daf6945984604ca20d0715732d27fc4d0c84c9
santinvalencia <santiamen0020@gmail.com>
2024-09-27 01:02:51 -0300

    sad

--- is.php
+++ is.php
@@ -3,7 +3,8 @@
                 <center>
                 <hr class="my-2">
                 <img class="display-3" width="200px" src="img/is.png">
-                <p class="lead">Juego de pelea 2D 1 contra 1</p>
+                <p class="lead">Juego de pelea 2D</p>
+                <p class="lead">1v1</p>
                 <p>Juega con tus amigos!</p>
                 <p class="lead">
                     <a class="btn btn-primary btn-lg" download="Infinitty_Showdown" href="src/juego.html" role="button">Jugar Ahora</a>
