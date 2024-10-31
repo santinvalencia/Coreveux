ca8d354717d01ddd59a611893e42272d0fd5b2ba
FabricioSalgueiro <salgueirofabricio6@gmail.com>
2024-10-31 02:43:19 -0300

    Pagina sobre Infinity Showdown
    asd

--- is.php
+++ is.php
@@ -4,7 +4,11 @@
                 <hr class="my-2">
                 <img class="display-3" width="200px" src="img/is.png">
                 <p class="lead">Juego de pelea 2D</p>
+                <p class="lead">Un juego FRENETICO al estilo Super Smash Bros donde tendras un campo para luchar libremente con tu amigo!!</p>
+                <p class="lead">Cada jugador tendra que esquivar los ataques de su oponente y intentara vencerlo hasta cierto limite de tiempo</p>
+                <p class="lead">Tambien apareceran objetos cada cierto intervalo de tiempo, que podran ayudar al jugador, estos pueden ser o armas o potenciadores</p>
                 <p class="lead">1v1</p>
+                <p class="lead">Al ser un juego 1v1, podras tener minutos, horas, dias, semanas, meses, años de diversion junto a tus amigos, con personajes nuevos e inovadores en el mundo del gaming, un juego ideal para socializar y pasarla bien con tus amigos, familiares, compañeros etc!!!</p>
                 <p>Juega con tus amigos!</p>
                 <p class="lead">
                     <a class="btn btn-primary btn-lg" download="Infinitty_Showdown" href="src/juego.html " role="button">Jugar Ahora</a>
