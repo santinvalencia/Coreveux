350533f326bccc92324bddb760584ccee3229f8b
santinvalencia <164514160+santinvalencia@users.noreply.github.com>
2024-11-29 05:10:02 -0300

    commit

--- template/footer.php
+++ template/footer.php
@@ -1,11 +1,13 @@
 
 <footer class="footer">
+        <ul></ul>
+        <ul></ul>
         <div class="row">
           <div class="col-lg-12">
             <ul class="list-unstyled">
-              <li><a href="https://github.com/santinvalencia/Coreveux">GitHub</a></li>
+              <li><a href="https://github.com/santinvalencia/Coreveux">GitHub Coreveux</a></li>
+              <li><a href="https://github.com/santinvalencia/IS">GitHub Infinitty Showndown</a></li>
             </ul>
-            <p>Made by <a href="https://thomaspark.co/">Coreveux Team</a>.</p>
             <a href="#top">Back to top</a>  
         </div>
         </div>
